8f483c13bd8de37eab78132470a2305b88b9f451
sanchit02102004 <126904961+sanchit02102004@users.noreply.github.com>
2023-03-14 23:21:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 
+// ----------nav bar--------
+menu=document.querySelector(".menu");
+menu.onclick=function(){
+    nav_section=document.querySelector(".nav-section");
+    nav_section.classList.toggle("active");
+}
